f17cbc3e26679a9a8041a82735f902f8bef734be
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-18 06:53:40 -0600

    Finished it
    Created a prime factors calculator using the sieve of eratosthenes.

--- lib/prime_factors_kata.rb
+++ lib/prime_factors_kata.rb
@@ -1,2 +1,30 @@
+require "prime"
+
 class PrimeFactorsKata
+
+  def eratosthenes(number)
+    candidates = [nil, nil, *2..number]
+    (2..Math.sqrt(number)).each do |i|
+      (i**2..number).step(i){|m| candidates[m] = nil} if candidates[i]
+    end
+    candidates.compact
+  end
+
+  def calculate(number)
+    resulting_factors = []
+    candidates = eratosthenes(number)
+    prime_candidates = candidates.to_enum
+    current_prime = prime_candidates.next
+    until Prime.prime?(number)
+      if number % current_prime == 0
+        resulting_factors << current_prime
+        number /= current_prime
+      else
+        current_prime = prime_candidates.next
+      end
+    end
+    resulting_factors << number
+    resulting_factors
+  end
+
 end
--- spec/prime_factors_kata_spec.rb
+++ spec/prime_factors_kata_spec.rb
@@ -3,11 +3,50 @@ require "prime_factors_kata"
 describe PrimeFactorsKata do
 
   before do
-
+    @prime_factors_kata = PrimeFactorsKata.new
   end
 
-  it "should return 2 for an input of 2" do
+  it "should return [2] for an input of 2" do
+    prime = @prime_factors_kata.calculate(2)
+    expect(prime).to eq [2]
+  end
 
+  it "should return [3] for an input of 3" do
+    prime = @prime_factors_kata.calculate(3)
+    expect(prime).to eq [3]
+  end
+
+  it "should return [2,2] for an input of 4" do
+    prime = @prime_factors_kata.calculate(4)
+    expect(prime).to eq [2,2]
+  end
+
+  it 'should return [2,2,2] for input of 8' do
+    prime = @prime_factors_kata.calculate(8)
+
+    expect(prime).to eq [2, 2, 2]
+  end
+
+  it "should return [3,3] for an input of 9" do
+    prime = @prime_factors_kata.calculate(9)
+    expect(prime).to eq [3,3]
+  end
+
+  it 'should return [2,7] for input of 14' do
+    prime = @prime_factors_kata.calculate(14)
+    expect(prime).to eq [2, 7]
+  end
+
+  it 'should handle any number' do
+    prime = @prime_factors_kata.calculate(168)
+
+    expect(prime).to eq [2, 2, 2, 3, 7]
+  end
+
+  it 'should handle any number' do
+    prime = @prime_factors_kata.calculate(330)
+
+    expect(prime).to eq [2, 3, 5, 11]
   end
 
 end
